8b6950a6d86f4db412df66cdab3a7d8c5bba3c1c
Wonbin-Choi <zsstarsz123@gmail.com>
2024-06-24 17:23:07 +0900

    4

---
+++ ch4/4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Sample{
+	int *p;
+	int size;
+public:
+	Sample(int n){
+		size = n; p = new int [n];
+	}
+	void read();
+	void write();
+	int big();
+	~Sample();
+};
+Sample::~Sample(){delete [] p;}
+void Sample::read(){
+	int num;
+	for(int i=0; i<size; i++){
+		cin >> num;
+		*(p+i) = num;
+	}
+}
+void Sample::write(){
+	for(int i=0;i<size;i++){
+		cout << p[i] << ' ';
+	}
+	cout << endl;
+}
+int Sample::big(){
+	int max = 0;
+	for(int i=0;i<size;i++){
+		if(max < p[i]) max = p[i];
+	}
+	return max;
+}
+int main(){
+	Sample s(10);
+	s.read();
+	s.write();
+	cout << "most big number " << s.big() << endl;
+	return 0;
+}
